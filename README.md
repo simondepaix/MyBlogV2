52cd4959e7714dfb126b3e7067384f8bb0c56dd9
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-12-15 10:30:09 +0100

    myblog-partie7

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# MyBlogV2
-Ce repo est un cours sur php poo mvc
+# Ce TP à pour but d'introduire au concept MVC et POO en php. Nous allons créer un blog en partant d'une base bootstrap.
+
+# MyBlog partie 7
+## consignes : 
+Halleluja, c'était pas simple mais on à réussi à récupérer les données depuis notre bdd et à les afficher grâce à un var_dump().
+Maintenant, nous allons bien sûr mettre en place ces data dans la vue home.
+- Créez une boucle foreach dans la vue home et mettez en forme les Post preview
+- Si vous utilisez PDO::FETCH_CLASS, que remarquez vous dans votre var_dump() ? En effet, PDO::FETCH_CLASS permet de retourner un objet de type PostModel, nous avons donc accès à toutes le méthodes de PostModel et donc des getters et setters !
+
---
+++ app/Controllers/MainController.php
@@ -0,0 +1,52 @@
+<?php
+class MainController{
+    protected $view;    
+    protected $data;
+    public function render(){          
+        require __DIR__.'/../Models/PostModel.php';
+        $postModel = new PostModel();        
+        $this->data = $postModel->getPosts(5);   
+        $data = $this->data;  
+        var_dump($data);
+        require __DIR__.'/../views/front/layouts/header.phtml';
+        require __DIR__."/../views/front/partials/$this->view.phtml";
+        require __DIR__.'/../views/front/layouts/footer.phtml';
+        
+    }
+
+    /**
+     * Get the value of view
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * Set the value of view
+     */
+    public function setView($view)
+    {
+        $this->view = $view;
+        
+    }
+
+
+
+    /**
+     * Get the value of data
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Set the value of data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+        
+    }
+}
