e95321cdbede85f6c3bd18f2601e4c2b062162ea
simondepaix <33658333+simondepaix@users.noreply.github.com>
2023-12-18 18:36:19 +0100

    myblog-partie10

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 class HomeController extends MainController{
 
     public function renderHome(){    
-        require __DIR__.'/../Models/PostModel.php';
+         require '../app/Models/PostModel.php';
         $postModel = new PostModel();        
         $this->data = $postModel->getPosts(5);   
         $this->render();        
--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -3,7 +3,7 @@
 class PostController extends MainController{
 
     public function renderPost(){
-        require __DIR__.'/../Models/PostModel.php';        
+        require '../app/Models/PostModel.php';        
         $postModel = new PostModel();                   
         $this->data =  $postModel->getPostById($this->id);        
         $this->render();
